2a47ed12b769c5620ad1ee5894537083ba071e25
Koay Chun Yong <koaychunyong@gmail.com>
2023-05-19 15:12:34 +0800

    Setting up react-router-dom

--- eTracker/src/App.tsx
+++ eTracker/src/App.tsx
@@ -1,24 +1,32 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Grid
-        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "side main"` }}
-        templateColumns={{ base: "1fr", lg: "1fr 2fr" }}
-      >
-        <GridItem bg="red" area={"nav"}>
-          Navbar
-        </GridItem>
-        <Show above="lg">
-          <GridItem bg="blue" area={"side"}>
-            Side
+      <Router>
+        <Grid
+          templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "side main"` }}
+          templateColumns={{ base: "1fr", lg: "1fr 2fr" }}
+        >
+          <GridItem bg="red" area={"nav"}>
+            Navbar
           </GridItem>
-        </Show>
-        <GridItem bg="green" area={"main"}>
-          Main
-        </GridItem>
-      </Grid>
+          <Show above="lg">
+            <GridItem bg="blue" area={"side"}>
+              Side
+            </GridItem>
+          </Show>
+          <Routes>
+            <GridItem bg="green" area={"main"}>
+              <Route path="/">Overview</Route>
+              <Route path="/task">Tasks List</Route>
+              <Route path="/history">Completed Tasks</Route>
+              <Route>Overview</Route>
+            </GridItem>
+          </Routes>
+        </Grid>
+      </Router>
     </>
   );
 }
