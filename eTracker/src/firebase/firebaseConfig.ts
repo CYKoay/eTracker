41eabcc102b113f97e23105d69a601ef224ef5b6
Koay Chun Yong <koaychunyong@gmail.com>
2023-05-19 10:34:21 +0800

    Setting up firebase and standardizing component naming

---
+++ eTracker/src/firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_F3b_RAyolJOba6yo1SY1sImab77Ipqg",
+  authDomain: "etracker-92eaf.firebaseapp.com",
+  projectId: "etracker-92eaf",
+  storageBucket: "etracker-92eaf.appspot.com",
+  messagingSenderId: "987353416844",
+  appId: "1:987353416844:web:45e30f6feb8efde3dba7a9",
+  measurementId: "G-XZZZ68PMNJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
